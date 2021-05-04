c4dbd0a96964a5fb3a5fe55790dc5523e6809d12
wata_mac <watassbass@gmail.com>
2021-05-04 16:54:07 +0900

    Step 27: Comments

--- test.sh
+++ test.sh
@@ -150,5 +150,8 @@ assert 98 'int main() { return "abc"[1]; }'
 assert 99 'int main() { return "abc"[2]; }'
 assert 0 'int main() { return "abc"[3]; }'
 assert 4 'int main() { return sizeof("abc"); }'
+try 2 'int main() { /* return 1; */ return 2; }'
+try 2 'int main() { // return 1;
+return 2; }'
 
 echo OK
--- tokenizer.c
+++ tokenizer.c
@@ -95,6 +95,21 @@ Token *tokenize(char *p) {
       continue;
     }
 
+    if (strncmp(p, "//", 2) == 0) {
+      p += 2;
+      while (*p != '\n')
+        p++;
+      continue;
+    }
+
+    if (strncmp(p, "/*", 2) == 0) {
+      char *q = strstr(p + 2, "*/");
+      if (!q)
+        error_at("Missing closing comment", cur->input);
+      p = q + 2;
+      continue;
+    }
+
     char *kw = starts_with_reserved(p);
     if (kw) {
       int len = strlen(kw);

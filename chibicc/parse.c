79edf27c9295d769c3de284990de072c03f915e6
wata_mac <watassbass@gmail.com>
2022-05-08 03:21:19 +0900

    Add unary & and *

--- chibicc/parse.c
+++ chibicc/parse.c
@@ -259,7 +259,7 @@ static Node *mul(Token **rest, Token *tok) {
   }
 }
 
-// unary = ("+" | "-") unary
+// unary = ("+" | "-" | "*" | "&") unary
 //       | primary
 static Node *unary(Token **rest, Token *tok) {
   if (equal(tok, "+"))
@@ -267,6 +267,12 @@ static Node *unary(Token **rest, Token *tok) {
 
   if (equal(tok, "-"))
     return new_unary(ND_NEG, unary(rest, tok->next), tok);
+
+  if (equal(tok, "&"))
+    return new_unary(ND_ADDR, unary(rest, tok->next), tok);
+
+  if (equal(tok, "*"))
+    return new_unary(ND_DEREF, unary(rest, tok->next), tok);
 
   return primary(rest, tok);
 }

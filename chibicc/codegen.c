016083d8cb5d78cc4202eae7ad3aab479a93aee5
wata_mac <watassbass@gmail.com>
2022-05-15 18:49:40 +0900

    Add global variables

--- chibicc/codegen.c
+++ chibicc/codegen.c
@@ -28,7 +28,11 @@ static int align_to(int n, int align) {
 static void gen_addr(Node *node) {
   switch (node->kind) {
   case ND_VAR:
-    printf("  lea %d(%%rbp), %%rax\n", node->var->offset);
+    if (node->var->is_local) {
+      printf("  lea %d(%%rbp), %%rax\n", node->var->offset);
+    } else {
+      printf("  lea %s(%%rip), %%rax\n", node->var->name);
+    }
     return;
   case ND_DEREF:
     gen_expr(node->lhs);
@@ -195,9 +199,19 @@ static void assign_lvar_offsets(Obj *prog) {
   }
 }
 
-void codegen(Obj *prog) {
-  assign_lvar_offsets(prog);
+static void emit_data(Obj *prog) {
+  for (Obj *var = prog; var; var = var->next) {
+    if (var->is_function)
+      continue;
 
+    printf("  .data\n");
+    printf("  .globl %s\n", var->name);
+    printf("%s:\n", var->name);
+    printf("  .zero %d\n", var->ty->size);
+  }
+}
+
+static void emit_text(Obj *prog) {
   for (Obj *fn = prog; fn; fn = fn->next) {
     if (!fn->is_function)
       continue;
@@ -228,3 +242,10 @@ void codegen(Obj *prog) {
     printf("  ret\n");
   }
 }
+
+void codegen(Obj *prog) {
+  assign_lvar_offsets(prog);
+  emit_data(prog);
+  emit_text(prog);
+}
+

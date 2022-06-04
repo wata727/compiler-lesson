cd78e8d2a2ac3b923fe000e3853834c83809e093
Kazuma Watanabe <watassbass@gmail.com>
2022-06-04 09:29:28 +0000

    Support struct tags

--- parse.c
+++ parse.c
@@ -7,10 +7,18 @@ struct VarScope {
   Obj *var;
 };
 
+typedef struct TagScope TagScope;
+struct TagScope {
+  TagScope *next;
+  char *name;
+  Type *ty;
+};
+
 typedef struct Scope Scope;
 struct Scope {
   Scope *next;
   VarScope *vars;
+  TagScope *tags;
 };
 
 static Obj *locals;
@@ -50,6 +58,14 @@ static Obj *find_var(Token *tok) {
     for (VarScope *sc2 = sc->vars; sc2; sc2 = sc2->next)
       if (equal(tok, sc2->name))
         return sc2->var;
+  return NULL;
+}
+
+static Type *find_tag(Token *tok) {
+  for (Scope *sc = scope; sc; sc = sc->next)
+    for (TagScope *sc2 = sc->tags; sc2; sc2 = sc2->next)
+      if (equal(tok, sc2->name))
+        return sc2->ty;
   return NULL;
 }
 
@@ -142,6 +158,14 @@ static int get_number(Token *tok) {
   if (tok->kind != TK_NUM)
     error_tok(tok, "expected a number");
   return tok->val;
+}
+
+static void push_tag_scope(Token *tok, Type *ty) {
+  TagScope *sc = calloc(1, sizeof(TagScope));
+  sc->name = strndup(tok->loc, tok->len);
+  sc->ty = ty;
+  sc->next = scope->tags;
+  scope->tags = sc;
 }
 
 // declspec = "char" | "int" | struct-decl
@@ -554,13 +578,25 @@ static void struct_members(Token **rest, Token *tok, Type *ty) {
   ty->members = head.next;
 }
 
-// struct-decl = "{" struct-members
+// struct-decl = ident? "{" struct-members
 static Type *struct_decl(Token **rest, Token *tok) {
-  tok = skip(tok, "{");
+  Token *tag = NULL;
+  if (tok->kind == TK_IDENT) {
+    tag = tok;
+    tok = tok->next;
+  }
+
+  if (tag && !equal(tok, "{")) {
+    Type *ty = find_tag(tag);
+    if (!ty)
+      error_tok(tag, "unknown struct type");
+    *rest = tok;
+    return ty;
+  }
 
   Type *ty = calloc(1, sizeof(Type));
   ty->kind = TY_STRUCT;
-  struct_members(rest, tok, ty);
+  struct_members(rest, tok->next, ty);
   ty->align = 1;
 
   int offset = 0;
@@ -574,6 +610,8 @@ static Type *struct_decl(Token **rest, Token *tok) {
   }
   ty->size = align_to(offset, ty->align);
 
+  if (tag)
+    push_tag_scope(tag, ty);
   return ty;
 }
 

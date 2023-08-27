1005c13b281b1f8ba1ff49bf6fd067b73d8b6933
Kazuma Watanabe <watassbass@gmail.com>
2023-08-27 07:29:17 +0000

    Add a do-nothing preprocessor

--- main.c
+++ main.c
@@ -163,6 +163,7 @@ static void run_cc1(int argc, char **argv, char *input, char *output) {
 
 static void cc1(void) {
   Token *tok = tokenize_file(base_file);
+  tok = preprocess(tok);
   Obj *prog = parse(tok);
 
   FILE *out = open_file(output_file);
